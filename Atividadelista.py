8f39bcdd47a76579c5268c5bfa4c32f243357558
dayssonf <dayssonf@icloud.com>
2026-05-06 20:57:04 -0300

    Segunda parte

--- Atividadelista.py
+++ Atividadelista.py
@@ -1,9 +1,27 @@
 mercado = ["trigo", "acucar", "arroz", "feijao", "macarrao"]
-preco =  [20.0, 18.0, 23.30, 12.0, 6.99]
+preco =  [19.98, 18.00, 23.30, 12.00, 6.99]
 print(mercado)
 print(mercado[0]) # imprime "trigo"
 print(mercado[-1]) # imprime "acucar"
 print(len(mercado))
 
+#exiba o nome e o preço dele
+print(f"o {mercado[0]} custa R${preco[0]}.")
 
- 
+#remova o último produto
+mercado.remove(mercado[-1])
+preco.remove(preco[-1])
+
+#Para somar o preço de todos os produtos
+
+total = sum(preco)
+print(f"O total deu R${total}")
+
+#lógica condicional if/else para desconto:
+
+if total < 100:
+    exit()
+else:
+    desconto = 0.95
+    total = total * desconto
+    print(f"O total agora com deconto é de R$ {total}.")
