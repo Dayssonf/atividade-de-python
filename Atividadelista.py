7c2ca2deedc1e671f9ef297626d92d5a13746916
dayssonf <dayssonf@icloud.com>
2026-05-06 20:39:05 -0300

    Primeira parte

---
+++ Atividadelista.py
@@ -0,0 +1,9 @@
+mercado = ["trigo", "acucar", "arroz", "feijao", "macarrao"]
+preco =  [20.0, 18.0, 23.30, 12.0, 6.99]
+print(mercado)
+print(mercado[0]) # imprime "trigo"
+print(mercado[-1]) # imprime "acucar"
+print(len(mercado))
+
+
+ 
