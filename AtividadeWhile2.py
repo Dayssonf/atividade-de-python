2f9eec991ea9efe111430e337c61e90d1e251de9
dayssonf <dayssonf@icloud.com>
2026-05-14 21:47:33 -0300

    dificil demais pra mim

---
+++ AtividadeWhile2.py
@@ -0,0 +1,17 @@
+contador = 0
+numero = -1
+
+
+while numero != 0:
+    numero = int(input("Digite um número (O para sair): "))
+
+    if numero != 0:
+        contator = contador + 1
+
+print(f"Quantidade de numeros digitados:{contador}")
+        
+
+#Atividade 2 anotação
+# 1 Contador recebe 0
+# 2 Enquanto o Usuário não digitar 0
+# 3 pergunte "Digite um número" e converta para inteiro
