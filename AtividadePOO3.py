4ab8a244ce965b2b4e61929f995a94cf3b317c58
dayssonf <dayssonf@icloud.com>
2026-06-15 19:27:15 -0300

    tava pensando errado

--- AtividadePOO3.py
+++ AtividadePOO3.py
@@ -1,95 +1,50 @@
-class Aluno:
-    def __init__(self, nome):
+class Alunos:
+    def __init__(self, nome, notas):
+
         self.nome = nome
-        self.notas = []
+        self.notas = notas
 
     def exibir(self):
         print(f"Aluno: {self.nome}")
-
         if self.notas:
             for ordem_nota, nota in enumerate(self.notas, 1):
                 print(f"Nota Nº {ordem_nota}: {nota}")
-        else:
-            print("Sem notas cadastradas.")
 
-    def situacao(self):
-        if not self.notas:
-            print("Aluno sem notas.")
-            return
-
-        media = sum(self.notas) / len(self.notas)
-
-        print(f"Média: {media:.2f}")
-
-        if media < 7:
-            print("Reprovado!")
-        else:
-            print("Aprovado!")
-
-    @staticmethod
     def exibir_menu():
-        print("\n==========")
-        print("1 - Cadastrar Aluno")
-        print("2 - Lançar Nota")
-        print("3 - Ver Situação")
-        print("4 - Listar Alunos")
-        print("0 - Sair")
         print("==========")
-
-alunos = []
+        print("1- Cadastrar Aluno: ")
+        print("2- Lanças notas ")
+        print("3- Ver Situação: ")
+        print("4- Listar Alunos: ")
+        print("0 - Sair!")
+        print("==========")
+    
+    def situacao(self):
+        media = sum(self.notas) / len(self.notas)
+        if media <=6:
+            print("Reprovado!")
+        elif media >= 7 and media <=10:
+            print("Aprovado!")
+        else:
+            print("Número inválido...")
 
 def cadastrar_aluno():
-    nome = input("Digite o nome do aluno: ")
-    aluno = Aluno(nome)
-    alunos.append(aluno)
-    print(f"Aluno {nome} cadastrado com sucesso!")
-
+    nome = input("Digite o seu nome: ")
+    aluno = Alunos(nome)
+    aluno.append(aluno)
+    print(f"Aluno {nome} cadastrado com sucesso. ")
 def lancar_nota():
     indice = int(input("Digite o código do aluno: "))
-
-    if 0 <= indice < len(alunos):
-        nota = float(input("Digite a nota: "))
-        alunos[indice].notas.append(nota)
-        print("Nota cadastrada!")
-    else:
-        print("Aluno não encontrado.")
-
-def ver_situacao():
-    indice = int(input("Digite o código do aluno: "))
-
-    if 0 <= indice < len(alunos):
-        alunos[indice].situacao()
-    else:
-        print("Aluno não encontrado.")
-
-def listar_alunos():
-    if not alunos:
-        print("Nenhum aluno cadastrado.")
-        return
-
-    for i, aluno in enumerate(alunos):
-        print(f"\nCódigo: {i}")
-        aluno.exibir()
-
+    aluno = aluno[indice]
+    nota = float(input("Digite a nota para ser lançada: "))
+    aluno.notas.append(nota)
+    
+aluno = []
 while True:
-    Aluno.exibir_menu()
+    exibir_menu()
     opcao = input("Escolha uma opção: ")
 
-    if opcao == "1":
-        cadastrar_aluno()
-
-    elif opcao == "2":
-        lancar_nota()
-
-    elif opcao == "3":
-        ver_situacao()
-
-    elif opcao == "4":
-        listar_alunos()
-
-    elif opcao == "0":
-        print("Encerrando...")
-        break
-
-    else:
-        print("Opção inválida!")
+    if opcao == "0":
+        print("encerando...")
+    elif opcao == 1:
+        
